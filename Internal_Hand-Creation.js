ea4012796335fb5ae844f248028f44db773c8584
TimeCipher <louie.moore@ymail.com>
2023-03-28 22:19:45 +0100

    Introduced Usr and Skystone classes

--- Internal_Hand-Creation.js
+++ Internal_Hand-Creation.js
@@ -1,58 +1,57 @@
-//This will generate the hand keycode to later be verified by the server
-//User will send cards, and a unique code will be created for that hand
-//Another part of the server will then be able to verify and decode said hand
-
 // Card - Type - Top - Left - Bottom - Right
 // lance - nil - 01 -01 - 00 - 00
-
 
 const { WebSocketServer } = require('ws');
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 
-// Spinning the http server and the WebSocket server.
+
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
-const port = 9752;
-server.listen(port, () => {
-  console.log(`WebSocket server is running on port ${port}`);
+server.listen(9752, () => {
+  console.log(`WebSocket Server Running`);
 });
 
-// I'm maintaining all active connections in this object
-const clients = {};
+class Usr {
+  constructor(UserId, Connection) {
+    this.UserId = UserId;
+    this.Connection = Connection;
+  }
+}
 
-// A new client connection request received
+
+const Clients = {};
+
 wsServer.on('connection', function(connection) {
-    // Generate a unique code for every user
-    const userId = uuidv4();
     console.log(`Recieved a new connection.`);
+    var ConnectedUsr = new Usr(uuidv4(),connection)
 
-    // Store the new connection and handle messages
-    clients[userId] = connection;
-    console.log(`${userId} connected.`);
+    connection.on('close', () => function() {
+      console.log("Someone Disconnected.");
+      for (let i in Clients) {
+        if (Clients[i].UserId == ConnectedUsr.UserId) {
 
-    function handleDisconnect(userId) {
-        console.log(`${userId} disconnected.`);
-        delete clients[userId];
-    }
-    // User disconnected
-    connection.on('close', () => handleDisconnect(userId));
+        }
+      }
+    });
 
     connection.on('message', function message(data) {
         console.log('received: %s', data);
     });
 
-    connection.send("Hello")
+    connection.send("Hello");
 });
 
-function broadcastMessage(json) {
-    // We are sending the current data to all connected active clients
-    const data = JSON.stringify(json);
-    for(let userId in clients) {
-      let client = clients[userId];
-      if(client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    };
-}
 
+
+
+class Skystone {
+  constructor(Name,Element,Top,Left,Bottom,Right) {
+    this.Name = Name;
+    this.Element = Element;
+    this.Top = Top;
+    this.Left = Left;
+    this.Bottom = Bottom;
+    this.Right = Right;
+  }
+}
