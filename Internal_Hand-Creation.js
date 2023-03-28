b9eb9829f59499061f2655550ddd7b6182215f1d
TimeCipher <louie.moore@ymail.com>
2023-03-28 09:59:16 +0100

    Set up Websocket

--- Internal_Hand-Creation.js
+++ Internal_Hand-Creation.js
@@ -1,3 +1,33 @@
 //This will generate the hand keycode to later be verified by the server
 //User will send cards, and a unique code will be created for that hand
 //Another part of the server will then be able to verify and decode said hand
+
+// Card - Type - Top - Left - Bottom - Right
+// lance - nil - 01 -01 - 00 - 00
+
+
+const { WebSocketServer } = require('ws');
+const http = require('http');
+const { v4: uuidv4 } = require('uuid');
+
+// Spinning the http server and the WebSocket server.
+const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
+const port = 9752;
+server.listen(port, () => {
+  console.log(`WebSocket server is running on port ${port}`);
+});
+
+// I'm maintaining all active connections in this object
+const clients = {};
+
+// A new client connection request received
+wsServer.on('connection', function(connection) {
+  // Generate a unique code for every user
+  const userId = uuidv4();
+  console.log(`Recieved a new connection.`);
+
+  // Store the new connection and handle messages
+  clients[userId] = connection;
+  console.log(`${userId} connected.`);
+});
--- WebsiteMain/WebCore.js
+++ WebsiteMain/WebCore.js
@@ -2,3 +2,5 @@
 // Holy Grail of sky stones websites
 
 //Used to actually run the client of the webpage
+
+var Socket = new WebSocket("ws://127.0.0.1:9752/")
