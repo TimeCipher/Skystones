29f0992decb50f73ee10c89f79b6899d9d129bcd
TimeCipher <louie.moore@ymail.com>
2023-03-28 10:06:35 +0100

    Serverside close handling and partial message handling

--- Internal_Hand-Creation.js
+++ Internal_Hand-Creation.js
@@ -23,11 +23,38 @@ const clients = {};
 
 // A new client connection request received
 wsServer.on('connection', function(connection) {
-  // Generate a unique code for every user
-  const userId = uuidv4();
-  console.log(`Recieved a new connection.`);
+    // Generate a unique code for every user
+    const userId = uuidv4();
+    console.log(`Recieved a new connection.`);
 
-  // Store the new connection and handle messages
-  clients[userId] = connection;
-  console.log(`${userId} connected.`);
+    // Store the new connection and handle messages
+    clients[userId] = connection;
+    console.log(`${userId} connected.`);
+
+    function handleDisconnect(userId) {
+        console.log(`${userId} disconnected.`);
+        const json = { type: typesDef.USER_EVENT };
+        const username = users[userId]?.username || userId;
+        userActivity.push(`${username} left the document`);
+        json.data = { users, userActivity };
+        delete clients[userId];
+        delete users[userId];
+        broadcastMessage(json);
+    }
+
+    // User disconnected
+    connection.on('close', () => handleDisconnect(userId));
+
 });
+
+function broadcastMessage(json) {
+    // We are sending the current data to all connected active clients
+    const data = JSON.stringify(json);
+    for(let userId in clients) {
+      let client = clients[userId];
+      if(client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    };
+}
+
--- WebsiteMain/WebCore.js
+++ WebsiteMain/WebCore.js
@@ -4,3 +4,7 @@
 //Used to actually run the client of the webpage
 
 var Socket = new WebSocket("ws://127.0.0.1:9752/")
+
+Socket.onmessage = ({data}) => {
+    
+};
