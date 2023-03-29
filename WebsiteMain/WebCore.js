f05061b591d39542020cbcc0ca732d3cfbd89157
TimeCipher <louie.moore@ymail.com>
2023-03-29 14:27:49 +0100

    Handler for translating Object to Image Path

--- WebsiteMain/WebCore.js
+++ WebsiteMain/WebCore.js
@@ -3,6 +3,7 @@
 
 //Used to actually run the client of the webpage
 
+const { fs } = require('fs');
 var Socket = new WebSocket("ws://127.0.0.1:9752/")
 
 Socket.onmessage = ({data}) => {
@@ -39,19 +40,32 @@ function removeClass(el, className)
 }
 // ------------------------------------
 
+class Skystone {
+    constructor(Name,Element,Top,Left,Bottom,Right,Position) {
+      this.Name = Name;
+      this.Element = Element;
+      this.Top = Top;
+      this.Left = Left;
+      this.Bottom = Bottom;
+      this.Right = Right;
+      this.Position = Position
+    }
+}
+
 var Selected_SkyStone = null;
 
 function select_skystone_hand(elem) {
-    if (hasClass(elem, "skystone_hand_selected")) {return}
+    if (hasClass(elem, "skystone_hand_selected")) {return};
 
     console.log(elem);
     console.log(elem.src.split("/"));
 
     var StoneSource_Split = elem.src.split("/");
-    var StoneSource_Name = StoneSource_Split[StoneSource_Split.length - 1]
-    var StoneSource_Details = StoneSource_Name.split(".")[0].split("-")
+    var StoneSource_Name = StoneSource_Split[StoneSource_Split.length - 1];
+    var StoneSource_Details = StoneSource_Name.split(".")[0].split("-");
+    // console.log(StoneSource_Name, StoneSource_Details[2][0]);
 
-    console.log(StoneSource_Name, StoneSource_Details[2][0])
+    Selected_SkyStone = new Skystone(StoneSource_Details[1],null,StoneSource_Details[2][0],StoneSource_Details[2][1],StoneSource_Details[2][2],StoneSource_Details[2][3],"Hand");
 
     var menu = document.getElementsByTagName('img');
     for (var i = 0; menu[i]; i++) {
@@ -61,5 +75,9 @@ function select_skystone_hand(elem) {
     };
 
     addClass(elem, "skystone_hand_selected");
-};
+}
 
+function construct_image_src(SkystoneObj) {
+    var SrcStr = `skystone-${SkystoneObj.Name}-${SkystoneObj.Top}${SkystoneObj.Left}${SkystoneObj.Bottom}${SkystoneObj.Right}.png`;
+    return SrcStr;
+}
